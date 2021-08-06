cd5792eaa27ed22d63d36593ed8fa75f59f6d492
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-06 20:54:10 +0100

    setup commit

---
+++ portfolios/create.sql
@@ -0,0 +1,43 @@
+-- SET foreign_key_checks = 0;
+-- DROP TABLE IF EXISTS assets, clients, investments;
+-- SET foreign_key_checks = 1;
+
+CREATE TABLE IF NOT EXISTS assets (
+	asset_id INT AUTO_INCREMENT,
+    asset_class TEXT(30),
+    PRIMARY KEY (asset_id)
+); 
+
+CREATE TABLE IF NOT EXISTS clients (
+	c_id INT AUTO_INCREMENT, 
+    c_name TEXT(30),
+    c_Location VARCHAR(30),
+    c_mobile VARCHAR(20) NOT NULL,
+    c_email VARCHAR(320) NOT NULL,
+    contract_type TEXT(12),
+    UNIQUE (c_mobile),
+    UNIQUE (c_email),	
+    PRIMARY KEY (c_id)
+);
+
+CREATE TABLE IF NOT EXISTS investments (
+	id INT AUTO_INCREMENT, 
+	plaform VARCHAR(50),
+	date_listed DATE, 
+    amount DECIMAL(10,2) NOT NULL,
+    CHECK (amount > 0), 
+    investment_horizon TEXT(12),
+    client_id INT,
+    asset_id INT,
+    i_description VARCHAR(80) NOT NULL, 
+    UNIQUE (i_description),
+    CONSTRAINT FK_assetid
+    FOREIGN KEY (asset_id)
+    REFERENCES assets(asset_id), 
+    CONSTRAINT FK_clientid
+    FOREIGN KEY (client_id)
+    REFERENCES clients(c_id)
+    ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
+    
---
+++ portfolios/insert.sql
@@ -0,0 +1,35 @@
+-- SET FOREIGN_KEY_CHECKS = 0;
+-- TRUNCATE TABLE assets;
+-- TRUNCATE TABLE clients;
+-- SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT IGNORE INTO assets 
+	(asset_class)
+VALUES 
+	('Cash and cash equivalent'), 
+    ('Bonds'), 
+    ('Stocks'),
+    ('Real Estate'), 
+    ('Currency'),
+    ('Sinking Fund'),
+    ('Pension Fund'),
+    ('Long-term notes'), 
+    ('Inventory'), 
+    ('Art collection'),
+    ('Precious metals'), 
+    ('Rare earth metals'), 
+    ('Real Property'), 
+    ('Commodity'), 
+    ('Futures'), 
+    ('Infrastructure');
+    
+INSERT IGNORE INTO clients 
+	(c_name, c_location, c_mobile, c_email, contract_type)
+VALUES
+	('Sophia Joseph', 'Chicago', '+1 312 649 5541', 'thesophiejo@gmail.com', 'flexible'),
+    ('Faris Al-Zaabi', 'Dubai', '+971 50 5472957', 'FarisAl@hotmail.com', 'long-term'),
+    ('Chongxi Ma', 'Hong Kong', '+852 2360 3382', 'xima@baidu.cn', 'long-term'),
+    ('Williams Brown', 'New York', '+1 718 963 0375', 'williambr@brown.edu', 'short-term'),
+    ('Hazlina Wong', 'Singapore', '+65 8458 2573', 'won8lina@gmail.com', 'flexible'),
+    ('Mike Karlsson', 'Boston', '+1 617 918 4243', 'Mikesson-pr1v@gmail.com', 'flexible');
+    
