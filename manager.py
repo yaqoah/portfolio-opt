a06ead466e88d126e0f659273f27ad83de40861d
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-08 18:02:56 +0100

    Connect to server and Initiate database

--- manager.py
+++ manager.py
@@ -2,8 +2,11 @@ import os
 import getpass
 from dotenv import load_dotenv
 import mysql.connector as db
+from mysql.connector.errors import ProgrammingError
 
-load_dotenv()  # prepare environment variables
+# ::::::::::::::::::::::::::::::::::::::::::::::
+load_dotenv() # prepare environment variables
+# ::::::::::::::::::::::::::::::::::::::::::::::
 
 mysql_host = os.getenv('MYSQL_HOST')
 mysql_user = os.getenv("MYSQL_USER")
@@ -11,8 +14,37 @@ mysql_password = getpass.getpass(prompt='Password: ')
 mysql_database = os.getenv("MYSQL_DB")
 conn = db.connect(user=mysql_user,
                   password=mysql_password,
-                  host=mysql_host,
-                  database=mysql_database)
+                  host=mysql_host)
 cursor = conn.cursor()
 
-# ..
+
+def build():
+    cursor.execute('CREATE DATABASE portfolios_db;')
+    cursor.execute('USE portfolios_db;')
+
+    delimiter: list = [';', ';']
+
+    for k, cmds in enumerate([
+        open('./portfolios/create.sql'),
+        open('./portfolios/insert.sql')
+    ]):
+        for query in cmds.read().split(delimiter[k]):
+            if query:
+                cursor.execute(query + ';')
+
+    conn.commit()
+
+
+try:
+    cursor.execute('USE ' + mysql_database + ';')
+    #debug code
+    """
+    cursor.execute(
+        '''
+        DROP DATABASE IF EXISTS portfolio_db
+        USE ''' + mysql_database + ';'
+    )
+    """
+except ProgrammingError as pr:
+    if pr.errno == 1049:  # database doesn't exist
+        build()
