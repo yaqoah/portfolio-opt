b7e33916ab43a8e08aee40967cd371da48b3c252
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-11 23:16:18 +0100

    added few more calls

--- portfolios/advanced/queries/calls.sql
+++ portfolios/advanced/queries/calls.sql
@@ -1,121 +1,121 @@
-CALL add_investment ("Robinhood", 1, 3, "2021-07-02", 74.10, 0.0598, 0.0474)
-CALL add_investment ("Vanguard", 4, 11, "2021-07-21", 386.38, 0.0880, 8.4877)
-CALL add_investment ("Robinhood", 4, 2, "2018-12-11", 22.84, 0.1151, 68.3778)
-CALL add_investment ("Webull", 2, 3, "2018-12-19", 1575.43, 0.1590, 8.8385)
-CALL add_investment ("eToro", 5, 3, "2020-08-31", 1521.61, 0.2043, 2.9975)
-CALL add_investment ("Interactive Broker", 2, 3, "2019-04-22", 2063.65, 0.1331, 1.7152)
-CALL add_investment ("Webull", 5, 2, "2020-11-08", 1412.06, 0.2057, 2.8384)
-CALL add_investment ("Webull", 5, 3, "2020-12-26", 738.60, 0.1948, 5.0290)
-CALL add_investment ("Fundrise", 6, 4, "2019-07-18", 3767580.84, 0.0417, 0.2685)
-CALL add_investment ("Interactive Broker", 2, 3, "2020-05-14", 2464.21, 0.0843, 6.6299)
-CALL add_investment ("eToro", 5, 2, "2020-04-22", 1591.58, 0.1082, 5.7029)
-CALL add_investment ("Interactive Broker", 2, 10, "2020-05-31", 2663.65, 0.1331, 1.4490)
-CALL add_investment ("Webull", 5, 2, "2020-12-29", 1477.70, 0.2286, 3.3373)
-CALL add_investment ("Forex", 4, 5, "2021-08-09", 69.84, 0.1095, 3.0840)
-CALL add_investment ("Interactive Broker", 2, 3, "2019-10-26", 203.65, 0.1331, 2.4240)
-CALL add_investment ("Fundrise", 6, 4, "2019-03-25", 843104.01, 0.0293, 0.0163)
-CALL add_investment ("Robinhood", 4, 5, "2018-01-03", 59.36, 0.1805, 69.5493)
-CALL add_investment ("Interactive Broker", 2, 3, "2020-12-31", 38.41, 0.1331, 0.6414)
-CALL add_investment ("eToro", 5, 2, "2021-05-29", 156.10, 0.0910, 10.5664)
-CALL add_investment ("Robinhood", 4, 5, "2019-06-21", 25.29, 0.1109, 35.2383)
-CALL add_investment ("eToro", 5, 2, "2020-04-22", 97.68, 0.2033, 8.7596)
-CALL add_investment ("Webull", 2, 3, "2020-07-05", 1932.64, 0.1535, 12.0320)
-CALL add_investment ("Robinhood", 1, 2, "2021-05-21", 375.30, 0.1583, 0.0853)
-CALL add_investment ("Forex", 4, 5, "2021-08-09", 16.42, 0.0505, 10.3642)
-CALL add_investment ("eToro", 5, 2, "2021-04-30", 1056.27, 0.2025, 12.6517)
-CALL add_investment ("Webull", 2, 3, "2020-08-11", 912.88, 0.0656, 25.3862)
-CALL add_investment ("Interactive Broker", 2, 2, "2019-10-26", 280.86, 0.1331, 1.2310)
-CALL add_investment ("Robinhood", 4, 3, "2021-06-21", 140.99, 0.2237, 12.8048)
-CALL add_investment ("Forex", 4, 5, "2021-07-18", 67.56, 0.1543, 11.4720)
-CALL add_investment ("Fundrise", 3, 4, "2015-09-18", 488642.78, 0.0354, 0.0418)
-CALL add_investment ("RD Ameritrade", 1, 2, "2021-07-24", 309.33, 0.0603, 0.0428)
-CALL add_investment ("CrowdStreet", 1, 4, "2021-02-15", 80379.52, 0.0568, 0.0043)
-CALL add_investment ("Robinhood", 4, 5, "2017-12-25", 72.30, 0.2289, 15.5551)
-CALL add_investment ("Fundrise", 6, 13, "2015-08-30", 5032023.10, 0.0486, 0.3265)
-CALL add_investment ("Webull", 5, 3, "2021-06-22", 611.33, 0.2420, 2.1678)
-CALL add_investment ("Forex", 4, 5, "2021-07-05", 28.36, 0.0749, 27.8287)
-CALL add_investment ("eToro", 3, 3, "2019-03-06", 55665.40, 0.0851, 0.9929)
-CALL add_investment ("Forex", 4, 5, "2021-07-23", 18.98, 0.0738, 27.8287)
-CALL add_investment ("Webull", 5, 2, "2020-11-16", 809.27, 0.1735, 4.3224)
-CALL add_investment ("Webull", 5, 2, "2020-02-04", 1588.55, 0.1402, 4.1689)
-CALL add_investment ("CrowdStreet", 5, 13, "2020-02-20", 7050.86, 0.0341, 0.1977)
-CALL add_investment ("Fundrise", 3, 4, "2015-04-27", 395739.81, 0.0383, 0.0538)
-CALL add_investment ("Vanguard", 3, 2, "2020-12-11", 52317.49, 0.1919, 0.9036)
-CALL add_investment ("Robinhood", 1, 2, "2021-06-24", 232.96, 0.1039, 0.0868)
-CALL add_investment ("RD Ameritrade", 1, 3, "2021-04-14",269.28, 0.0740, 0.0428)
-CALL add_investment ("Vanguard", 3, 3, "2019-08-04 ", 19045.21, 0.1913, 1.2475)
-CALL add_investment ("RD Ameritrade", 1, 2, "2021-04-12", 105.97, 0.1331, 0.0423)
-CALL add_investment ("Forex", 1, 5, "2021-05-31", 289.31, 0.0775, 35.5497)
-CALL add_investment ("Vanguard", 4, 11, "2021-07-29", 581.27, 0.1611, 13.2603)
-CALL add_investment ("CrowdStreet", 1, 13, "2021-05-28", 42672.37, 0.0603, 0.0043)
-CALL add_investment ("Vanguard", 4, 3, "2021-07-03", 224.27, 0.0880, 1.1303)
-CALL add_investment ("CrowdStreet", 3, 4, "2016-03-12", 258419.03, 0.0566, 0.0031)
-CALL add_investment ("RD Ameritrade", 4, 3, "2021-07-18", 328.06, 0.1131, 2.4390)
-CALL add_investment ("RD Ameritrade", 1, 11, "2021-06-18", 158.51, 0.1331, 0.0880)
-
-CALL add_investment ("Robinhood", 1, 2, "2021-04-19", 426.10, 0.0538, 0.0428)
-CALL add_investment ("Fundrise", 2, 13, "2019-05-20", 4008.89, 0.0519, 0.0049)
-CALL add_investment ("Interactive Broker", 2, 3, "2020-05-31", 34.11, 0.1331, 0.6414)
-CALL add_investment ("Forex", 4, 5, "2021-08-09", 165.41, 0.0924, 12.4828)
-CALL add_investment ("Forex", 1, 5, "2021-02-27", 377.23, 0.0479, 52.0898)
-CALL add_investment ("Webull", 5, 3, "2020-11-17", 807.77, 0.1679, 10.2292)
-CALL add_investment ("Robinhood", 4, 3, "2018-01-03", 27.61, 0.2216, 65.0477)
-CALL add_investment ("Interactive Broker", 2, 2, "2021-03-23", 29.97, 0.1331, 1.6505)
-CALL add_investment ("CrowdStreet", 3, 4, "2009-04-09", 540814.98, 0.0222, 0.0376)
-CALL add_investment ("Fundrise", 6, 13, "2017-06-12", 2170853.49, 0.0456, 0.1403)
-CALL add_investment ("Robinhood", 4, 2, "2021-05-21", 86.55, 0.2044, 0.0321)
-CALL add_investment ("Fundrise", 6, 4, "2016-07-12", 1518361.33, 0.0666, 0.3410)
-CALL add_investment ("RD Ameritrade", 1, 2, "2021-02-01", 327.79, 0.0792, 0.0428)
-CALL add_investment ("Vanguard", 3, 3, "2018-04-29", 16858.70, 0.1900, 2.6748)
-CALL add_investment ("Webull", 5, 3, "2021-04-17", 950.69, 0.1822, 7.9957)
-CALL add_investment ("Vanguard", 4, 2, "2021-07-25", 448.94, 0.2289, 9.1178)
-CALL add_investment ("eToro", 5, 2, "2020-11-27", 738.18, 0.1945, 8.7036)
-CALL add_investment ("CrowdStreet", 3, 4, "2008-09-07", 20716.21, 0.0383, 0.0754)
-CALL add_investment ("eToro", 5, 2, "2021-06-26", 933.78, 0.1528, 4.3698)
-CALL add_investment ("Robinhood", 1, 3, "2021-03-23", 64.59, 0.2445, 0.0455)
-CALL add_investment ("Vanguard", 3, 12, "2018-11-09", 19249.37, 0.1061, 0.5106)
-CALL add_investment ("Webull", 2, 3, "2019-04-27", 2387.51, 0.0867, 15.0052)
-CALL add_investment ("CrowdStreet", 3, 4, "2009-03-09", 522539.10, 0.0648, 0.0341)
-CALL add_investment ("Forex", 4, 5, "2021-08-08", 68.63, 0.1658, 32.2156)
-CALL add_investment ("eToro", 5, 3, "2020-02-07", 1501.40, 0.1616, 10.9120)
-CALL add_investment ("Vanguard", 3, 12, "2017-10-01", 39126.41, 0.1061, 2.7153)
-CALL add_investment ("RD Ameritrade", 4, 3, "2021-07-13", 594.80, 0.1692, 7.5808)
-CALL add_investment ("RD Ameritrade", 4, 12, "2021-07-24", 426.65, 0.1662, 3.3548)
-CALL add_investment ("Forex", 4, 5, "2021-07-23", 47.36, 0.0502, 13.1917)
-CALL add_investment ("Robinhood", 1, 2, "2021-07-29", 485.03, 0.1756, 0.0419)
-CALL add_investment ("Fundrise", 2, 4, "2020-04-27", 5627.23, 0.0663, 0.0034)
-CALL add_investment ("RD Ameritrade", 1, 2, "2021-06-22", 735.69, 0.1331, 0.0439)
-CALL add_investment ("RD Ameritrade", 4, 2, "2021-07-20", 179.06, 0.1236, 7.7124)
-CALL add_investment ("Forex", 4, 5, "2021-07-11", 77.67, 0.0983, 7.4722)
-CALL add_investment ("Robinhood", 1, 3, "2021-02-15", 87.71, 0.1583, 0.0330)
-CALL add_investment ("CrowdStreet", 1, 4, "2021-02-15", 86281.49, 0.0305, 0.0065)
-CALL add_investment ("Webull", 5, 2, "2020-07-24", 921.25, 0.1426, 12.0239)
-CALL add_investment ("Webull", 2, 3, "2020-04-11", 768.78, 0.0792, 0.0428)
-CALL add_investment ("Fundrise", 6, 4, "2018-03-21", 4083495.55, 0.0304, 0.2685)
-CALL add_investment ("Fundrise", 2, 4, "2019-05-25", 4968.38, 0.0429, 0.0055)
-CALL add_investment ("Fundrise", 6, 13, "2016-11-03", 3954258.04, 0.0269, 0.0433)
-CALL add_investment ("eToro", 5, 2, "2020-02-02", 72.04, 0.1486, 3.9154)
-CALL add_investment ("RD Ameritrade", 4, 3, "2021-06-14", 474.87, 0.1253, 7.9446)
-CALL add_investment ("CrowdStreet", 5, 13, "2021-02-12", 12004.64, 0.0608, 0.0350)
-CALL add_investment ("CrowdStreet", 5, 4, "2021-06-28", 15707.48, 0.0226, 0.0350)
-CALL add_investment ("eToro", 3, 3, "2019-09-30", 38641.90, 0.1191, 1.8049)
-CALL add_investment ("Vanguard", 3, 2, "2019-08-21", 56417.55, 0.1061, 2.2598)
-CALL add_investment ("Interactive Broker", 2, 2, "2019-03-23", 316.01, 0.0792, 25.3862)
-CALL add_investment ("Vanguard", 4, 3, "2021-07-16", 360.57, 0.1230, 0.3874)
-CALL add_investment ("Forex", 4, 5, "2021-07-23", 11.53, 0.1446, 3.2386)
-CALL add_investment ("Interactive Broker", 2, 3, "2021-01-16 ", 478.70, 0.2231, 9.6726)
-CALL add_investment ("Robinhood", 4, 3, "2021-07-02", 411.30, 0.0581, 20.1066)
-CALL add_investment ("Fundrise", 2, 4, "2019-01-02 ", 3888.10, 0.0395, 0.0024)
-CALL add_investment ("Webull", 5, 2, "2020-11-15", 631.23, 0.0819, 4.3224)
-CALL add_investment ("eToro", 3, 3, "2019-10-12", 49636.15, 0.0867, 0.2042)
-CALL add_investment ("RD Ameritrade", 4, 3, "2021-05-11", 63.41, 0.0708, 20.0599)
-CALL add_investment ("CrowdStreet", 3, 4, "2008-06-01 ", 285157.56, 0.0432, 0.0031)
-CALL add_investment ("Robinhood", 4, 2, "2021-07-16", 384.38, 0.0859, 0.0172)
-CALL add_investment ("Fundrise", 3, 4, "2013-06-24", 116924.89, 0.0428, 0.0117)
-CALL add_investment ("RD Ameritrade", 1, 3, "2021-05-30 ", 323.04, 0.1010, 0.0428)
-CALL add_investment ("eToro", 5, 2, "2020-04-22", 141.54, 0.1461, 8.7596)
-CALL add_investment ("Fundrise", 3, 4, "2010-07-01", 438634.30, 0.0445, 0.0618)
-CALL add_investment ("Forex", 1, 5, "2021-03-22", 267.42, 0.1528, 41.9374)
-CALL add_investment ("Interactive Broker", 2, 2, "2021-03-08", 308.59, 0.0656, 25.3862)
-CALL add_investment ("Robinhood", 4, 3, "2021-07-29", 333.98, 0.0598, 15.4615)
-CALL add_investment ("eToro", 5, 2, "2021-06-22", 1223.80, 0.1616, 9.4225)
+CALL add_investment ("Robinhood", 1, 3, "2021-07-02", 74.10, 0.0598, 0.0474);
+CALL add_investment ("Vanguard", 4, 11, "2021-07-21", 386.38, 0.0880, 8.4877);
+CALL add_investment ("Robinhood", 4, 2, "2018-12-11", 22.84, 0.1151, 68.3778);
+CALL add_investment ("Webull", 2, 3, "2018-12-19", 1575.43, 0.1590, 8.8385);
+CALL add_investment ("eToro", 5, 3, "2020-08-31", 1521.61, 0.2043, 2.9975);
+CALL add_investment ("Interactive Broker", 2, 3, "2019-04-22", 2063.65, 0.1331, 1.7152);
+CALL add_investment ("Webull", 5, 2, "2020-11-08", 1412.06, 0.2057, 2.8384);
+CALL add_investment ("Webull", 5, 3, "2020-12-26", 738.60, 0.1948, 5.0290);
+CALL add_investment ("Fundrise", 6, 4, "2019-07-18", 3767580.84, 0.0417, 0.2685);
+CALL add_investment ("Interactive Broker", 2, 3, "2020-05-14", 2464.21, 0.0843, 6.6299);
+CALL add_investment ("eToro", 5, 2, "2020-04-22", 1591.58, 0.1082, 5.7029);
+CALL add_investment ("Interactive Broker", 2, 10, "2020-05-31", 2663.65, 0.1331, 1.4490);
+CALL add_investment ("Webull", 5, 2, "2020-12-29", 1477.70, 0.2286, 3.3373);
+CALL add_investment ("Forex", 4, 5, "2021-08-09", 69.84, 0.1095, 3.0840);
+CALL add_investment ("Interactive Broker", 2, 3, "2019-10-26", 203.65, 0.1331, 2.4240);
+CALL add_investment ("Fundrise", 6, 4, "2019-03-25", 843104.01, 0.0293, 0.0163);
+CALL add_investment ("Robinhood", 4, 5, "2018-01-03", 59.36, 0.1805, 69.5493);
+CALL add_investment ("Interactive Broker", 2, 3, "2020-12-31", 38.41, 0.1331, 0.6414);
+CALL add_investment ("eToro", 5, 2, "2021-05-29", 156.10, 0.0910, 10.5664);
+CALL add_investment ("Robinhood", 4, 5, "2019-06-21", 25.29, 0.1109, 35.2383);
+CALL add_investment ("eToro", 5, 2, "2020-04-22", 97.68, 0.2033, 8.7596);
+CALL add_investment ("Webull", 2, 3, "2020-07-05", 1932.64, 0.1535, 12.0320);
+CALL add_investment ("Robinhood", 1, 2, "2021-05-21", 375.30, 0.1583, 0.0853);
+CALL add_investment ("Forex", 4, 5, "2021-08-09", 16.42, 0.0505, 10.3642);
+CALL add_investment ("eToro", 5, 2, "2021-04-30", 1056.27, 0.2025, 12.6517);
+CALL add_investment ("Webull", 2, 3, "2020-08-11", 912.88, 0.0656, 25.3862);
+CALL add_investment ("Interactive Broker", 2, 2, "2019-10-26", 280.86, 0.1331, 1.2310);
+CALL add_investment ("Robinhood", 4, 3, "2021-06-21", 140.99, 0.2237, 12.8048);
+CALL add_investment ("Forex", 4, 5, "2021-07-18", 67.56, 0.1543, 11.4720);
+CALL add_investment ("Fundrise", 3, 4, "2015-09-18", 488642.78, 0.0354, 0.0418);
+CALL add_investment ("RD Ameritrade", 1, 2, "2021-07-24", 309.33, 0.0603, 0.0428);
+CALL add_investment ("CrowdStreet", 1, 4, "2021-02-15", 80379.52, 0.0568, 0.0043);
+CALL add_investment ("Robinhood", 4, 5, "2017-12-25", 72.30, 0.2289, 15.5551);
+CALL add_investment ("Fundrise", 6, 13, "2015-08-30", 5032023.10, 0.0486, 0.3265);
+CALL add_investment ("Webull", 5, 3, "2021-06-22", 611.33, 0.2420, 2.1678);
+CALL add_investment ("Forex", 4, 5, "2021-07-05", 28.36, 0.0749, 27.8287);
+CALL add_investment ("eToro", 3, 3, "2019-03-06", 55665.40, 0.0851, 0.9929);
+CALL add_investment ("Forex", 4, 5, "2021-07-23", 18.98, 0.0738, 27.8287);
+CALL add_investment ("Webull", 5, 2, "2020-11-16", 809.27, 0.1735, 4.3224);
+CALL add_investment ("Webull", 5, 2, "2020-02-04", 1588.55, 0.1402, 4.1689);
+CALL add_investment ("CrowdStreet", 5, 13, "2020-02-20", 7050.86, 0.0341, 0.1977);
+CALL add_investment ("Fundrise", 3, 4, "2015-04-27", 395739.81, 0.0383, 0.0538);
+CALL add_investment ("Vanguard", 3, 2, "2020-12-11", 52317.49, 0.1919, 0.9036);
+CALL add_investment ("Robinhood", 1, 2, "2021-06-24", 232.96, 0.1039, 0.0868);
+CALL add_investment ("RD Ameritrade", 1, 3, "2021-04-14",269.28, 0.0740, 0.0428);
+CALL add_investment ("Vanguard", 3, 3, "2019-08-04 ", 19045.21, 0.1913, 1.2475);
+CALL add_investment ("RD Ameritrade", 1, 2, "2021-04-12", 105.97, 0.1331, 0.0423);
+CALL add_investment ("Forex", 1, 5, "2021-05-31", 289.31, 0.0775, 35.5497);
+CALL add_investment ("Vanguard", 4, 11, "2021-07-29", 581.27, 0.1611, 13.2603);
+CALL add_investment ("CrowdStreet", 1, 13, "2021-05-28", 42672.37, 0.0603, 0.0043);
+CALL add_investment ("Vanguard", 4, 3, "2021-07-03", 224.27, 0.0880, 1.1303);
+CALL add_investment ("CrowdStreet", 3, 4, "2016-03-12", 258419.03, 0.0566, 0.0031);
+CALL add_investment ("RD Ameritrade", 4, 3, "2021-07-18", 328.06, 0.1131, 2.4390);
+CALL add_investment ("RD Ameritrade", 1, 11, "2021-06-18", 158.51, 0.1331, 0.0880);
+CALL add_investment ("Robinhood", 1, 2, "2021-04-19", 426.10, 0.0538, 0.0428);
+CALL add_investment ("Fundrise", 2, 13, "2019-05-20", 4008.89, 0.0519, 0.0049);
+CALL add_investment ("Interactive Broker", 2, 3, "2020-05-31", 34.11, 0.1331, 0.6414);
+CALL add_investment ("Forex", 4, 5, "2021-08-09", 165.41, 0.0924, 12.4828);
+CALL add_investment ("Forex", 1, 5, "2021-02-27", 377.23, 0.0479, 52.0898);
+CALL add_investment ("Webull", 5, 3, "2020-11-17", 807.77, 0.1679, 10.2292);
+CALL add_investment ("Robinhood", 4, 3, "2018-01-03", 27.61, 0.2216, 65.0477);
+CALL add_investment ("Interactive Broker", 2, 2, "2021-03-23", 29.97, 0.1331, 1.6505);
+CALL add_investment ("CrowdStreet", 3, 4, "2009-04-09", 540814.98, 0.0222, 0.0376);
+CALL add_investment ("Fundrise", 6, 13, "2017-06-12", 2170853.49, 0.0456, 0.1403);
+CALL add_investment ("Robinhood", 4, 2, "2021-05-21", 86.55, 0.2044, 0.0321);
+CALL add_investment ("Fundrise", 6, 4, "2016-07-12", 1518361.33, 0.0666, 0.3410);
+CALL add_investment ("RD Ameritrade", 1, 2, "2021-02-01", 327.79, 0.0792, 0.0428);
+CALL add_investment ("Vanguard", 3, 3, "2018-04-29", 16858.70, 0.1900, 2.6748);
+CALL add_investment ("Webull", 5, 3, "2021-04-17", 950.69, 0.1822, 7.9957);
+CALL add_investment ("Vanguard", 4, 2, "2021-07-25", 448.94, 0.2289, 9.1178);
+CALL add_investment ("eToro", 5, 2, "2020-11-27", 738.18, 0.1945, 8.7036);
+CALL add_investment ("CrowdStreet", 3, 4, "2008-09-07", 20716.21, 0.0383, 0.0754);
+CALL add_investment ("eToro", 5, 2, "2021-06-26", 933.78, 0.1528, 4.3698);
+CALL add_investment ("Robinhood", 1, 3, "2021-03-23", 64.59, 0.2445, 0.0455);
+CALL add_investment ("Vanguard", 3, 12, "2018-11-09", 19249.37, 0.1061, 0.5106);
+CALL add_investment ("Webull", 2, 3, "2019-04-27", 2387.51, 0.0867, 15.0052);
+CALL add_investment ("CrowdStreet", 3, 4, "2009-03-09", 522539.10, 0.0648, 0.0341);
+CALL add_investment ("Forex", 4, 5, "2021-08-08", 68.63, 0.1658, 32.2156);
+CALL add_investment ("eToro", 5, 3, "2020-02-07", 1501.40, 0.1616, 10.9120);
+CALL add_investment ("Vanguard", 3, 12, "2017-10-01", 39126.41, 0.1061, 2.7153);
+CALL add_investment ("RD Ameritrade", 4, 3, "2021-07-13", 594.80, 0.1692, 7.5808);
+CALL add_investment ("RD Ameritrade", 4, 12, "2021-07-24", 426.65, 0.1662, 3.3548);
+CALL add_investment ("Forex", 4, 5, "2021-07-23", 47.36, 0.0502, 13.1917);
+CALL add_investment ("Robinhood", 1, 2, "2021-07-29", 485.03, 0.1756, 0.0419);
+CALL add_investment ("Fundrise", 2, 4, "2020-04-27", 5627.23, 0.0663, 0.0034);
+CALL add_investment ("RD Ameritrade", 1, 2, "2021-06-22", 735.69, 0.1331, 0.0439);
+CALL add_investment ("RD Ameritrade", 4, 2, "2021-07-20", 179.06, 0.1236, 7.7124);
+CALL add_investment ("Forex", 4, 5, "2021-07-11", 77.67, 0.0983, 7.4722);
+CALL add_investment ("Robinhood", 1, 3, "2021-02-15", 87.71, 0.1583, 0.0330);
+CALL add_investment ("CrowdStreet", 1, 4, "2021-02-15", 86281.49, 0.0305, 0.0065);
+CALL add_investment ("Webull", 5, 2, "2020-07-24", 921.25, 0.1426, 12.0239);
+CALL add_investment ("Webull", 2, 3, "2020-04-11", 768.78, 0.0792, 0.0428);
+CALL add_investment ("Fundrise", 6, 4, "2018-03-21", 4083495.55, 0.0304, 0.2685);
+CALL add_investment ("Fundrise", 2, 4, "2019-05-25", 4968.38, 0.0429, 0.0055);
+CALL add_investment ("Fundrise", 6, 13, "2016-11-03", 3954258.04, 0.0269, 0.0433);
+CALL add_investment ("eToro", 5, 2, "2020-02-02", 72.04, 0.1486, 3.9154);
+CALL add_investment ("RD Ameritrade", 4, 3, "2021-06-14", 474.87, 0.1253, 7.9446);
+CALL add_investment ("CrowdStreet", 5, 13, "2021-02-12", 12004.64, 0.0608, 0.0350);
+CALL add_investment ("CrowdStreet", 5, 4, "2021-06-28", 15707.48, 0.0226, 0.0350);
+CALL add_investment ("eToro", 3, 3, "2019-09-30", 38641.90, 0.1191, 1.8049);
+CALL add_investment ("Vanguard", 3, 2, "2019-08-21", 56417.55, 0.1061, 2.2598);
+CALL add_investment ("Interactive Broker", 2, 2, "2019-03-23", 316.01, 0.0792, 25.3862);
+CALL add_investment ("Vanguard", 4, 3, "2021-07-16", 360.57, 0.1230, 0.3874);
+CALL add_investment ("Forex", 4, 5, "2021-07-23", 11.53, 0.1446, 3.2386);
+CALL add_investment ("Interactive Broker", 2, 3, "2021-01-16 ", 478.70, 0.2231, 9.6726);
+CALL add_investment ("Robinhood", 4, 3, "2021-07-02", 411.30, 0.0581, 20.1066);
+CALL add_investment ("Fundrise", 2, 4, "2019-01-02 ", 3888.10, 0.0395, 0.0024);
+CALL add_investment ("Webull", 5, 2, "2020-11-15", 631.23, 0.0819, 4.3224);
+CALL add_investment ("eToro", 3, 3, "2019-10-12", 49636.15, 0.0867, 0.2042);
+CALL add_investment ("RD Ameritrade", 4, 3, "2021-05-11", 63.41, 0.0708, 20.0599);
+CALL add_investment ("CrowdStreet", 3, 4, "2008-06-01 ", 285157.56, 0.0432, 0.0031);
+CALL add_investment ("Robinhood", 4, 2, "2021-07-16", 384.38, 0.0859, 0.0172);
+CALL add_investment ("Fundrise", 3, 4, "2013-06-24", 116924.89, 0.0428, 0.0117);
+CALL add_investment ("RD Ameritrade", 1, 3, "2021-05-30 ", 323.04, 0.1010, 0.0428);
+CALL add_investment ("eToro", 5, 2, "2020-04-22", 141.54, 0.1461, 8.7596);
+CALL add_investment ("Fundrise", 3, 4, "2010-07-01", 438634.30, 0.0445, 0.0618);
+CALL add_investment ("Forex", 1, 5, "2021-03-22", 267.42, 0.1528, 41.9374);
+CALL add_investment ("Interactive Broker", 2, 2, "2021-03-08", 308.59, 0.0656, 25.3862);
+CALL add_investment ("Robinhood", 4, 3, "2021-07-29", 333.98, 0.0598, 15.4615);
+CALL add_investment ("eToro", 5, 2, "2021-06-22", 1223.80, 0.1616, 9.4225);
+CALL delete_client (1, 'Sophia Joseph')
