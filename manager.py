493411869d2a6828cc81e0cd288fbe0ca94a222b
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-12 17:19:53 +0100

    Client class + methods

--- manager.py
+++ manager.py
@@ -10,7 +10,8 @@ load_dotenv()  # prepare environment variables
 
 mysql_host = os.getenv('MYSQL_HOST')
 mysql_user = os.getenv("MYSQL_USER")
-mysql_password = getpass.getpass(prompt='Password: ')
+mysql_password = "juniorito11"
+# getpass.getpass(prompt='Password: ')
 mysql_database = os.getenv("MYSQL_DB")
 conn = db.connect(user=mysql_user,
                   password=mysql_password,
@@ -22,16 +23,20 @@ def build():
     cursor.execute('CREATE DATABASE portfolios_db;')
     cursor.execute('USE portfolios_db;')
 
-    delimiter: list = [';', ';', '$$', ]
+    delimiter: list = [';', ';', '$$', ";"]
 
     for k, cmds in enumerate([
         open('./portfolios/create.sql'),
         open('./portfolios/insert.sql'),
-        open('./portfolios/advanced/queries/procedures.sql')
+        open('./portfolios/advanced/queries/procedures.sql'),
+        open('./portfolios/advanced/queries/calls.sql')
     ]):
         for query in cmds.read().split(delimiter[k]):
-            if query:
+            try:
                 cursor.execute(query + ';')
+            except ProgrammingError as blank:
+                if blank.errno == 1065:
+                    continue
 
     conn.commit()
 
@@ -42,3 +47,26 @@ try:
 except ProgrammingError as pr:
     if pr.errno == 1049:  # database doesn't exist
         build()
+
+
+class Client:
+    @staticmethod
+    def investments_count(id: int):
+        query_all = cursor.callproc('all_client_investments', [id])
+        for investments in cursor.stored_results():
+            investments.fetchall()
+
+        return investments.rowcount
+
+    @staticmethod
+    def assets_count(id: int):
+        total_aclasses = [count for count in
+                     cursor.callproc('all_asset_classes', [id])][0]
+        client_classes = [c_count for c_count in
+                     cursor.callproc('client_asset_classes', [id, id])][0]
+        return total_aclasses, client_classes
+
+
+print(Client.investments_count(6))
+print(Client.assets_count(4))
+# code for diversification method
