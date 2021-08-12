e327c8d2cd1712dd83f0cc02f4c852b58d9a4b38
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-12 23:11:37 +0100

    Diversity CHECK methods and enum class

--- manager.py
+++ manager.py
@@ -1,11 +1,13 @@
 import os
 import getpass
+from enum import Enum
+from itertools import repeat
 from dotenv import load_dotenv
 import mysql.connector as db
 from mysql.connector.errors import ProgrammingError
 
 # ::::::::::::::::::::::::::::::::::::::::::::::
-load_dotenv()  # prepare environment variables
+load_dotenv()  # prepares environment variables
 # ::::::::::::::::::::::::::::::::::::::::::::::
 
 mysql_host = os.getenv('MYSQL_HOST')
@@ -51,22 +53,59 @@ except ProgrammingError as pr:
 
 class Client:
     @staticmethod
-    def investments_count(id: int):
-        query_all = cursor.callproc('all_client_investments', [id])
+    def investments_count(client_id: int):
+        query_all = cursor.callproc('all_client_investments', [client_id])
         for investments in cursor.stored_results():
             investments.fetchall()
 
         return investments.rowcount
 
     @staticmethod
-    def assets_count(id: int):
+    def assets_count(client_id: int):
         total_aclasses = [count for count in
-                     cursor.callproc('all_asset_classes', [id])][0]
+                          cursor.callproc( 'all_asset_classes',
+                                           [client_id])][0]
         client_classes = [c_count for c_count in
-                     cursor.callproc('client_asset_classes', [id, id])][0]
+                          cursor.callproc('client_asset_classes',
+                                          list(repeat(client_id, 2)))][0]
+
         return total_aclasses, client_classes
 
 
-print(Client.investments_count(6))
-print(Client.assets_count(4))
-# code for diversification method
+class Levels(Enum):
+    POOR = '\033[91m' + 'poorly diverse' + '\033[0m'
+    FAIR = '\033[1m' + 'insufficiently diverse' + '\033[0m'
+    GOOD = '\033[4m' + 'just about diverse' + '\033[0m'
+    VERY_GOOD = '\033[93m' + "well diverse" + '\033[0m'
+    EXCELLENT = '\033[92m' + "highly diverse" + '\033[0m'
+
+
+def diversity(client_id: int):
+    total_volatility = cursor.callproc('find_volatility', list(repeat(client_id, 2)))[1]
+    stakes = Client.investments_count(client_id)
+    classes = Client.assets_count(client_id)
+    diversity = (total_volatility/stakes) * 100
+
+    if stakes in range(22, 200) and classes[1] >= 3:
+        nominal_diversity = Levels.EXCELLENT.value
+    elif stakes in range(18, 25) and classes[1] in range(2, 4):
+        nominal_diversity = Levels.VERY_GOOD.value
+    elif stakes in range(15, 18) and classes[1] in range(4, 16):
+        nominal_diversity = Levels.GOOD.value
+    elif stakes in range(9, 15):
+        nominal_diversity = Levels.FAIR.value
+    else:
+        nominal_diversity = Levels.POOR.value
+
+    return (
+            f"(id:{client_id}) Client's portfolio volatility is {round(diversity,3)}%\n"
+            f"The volatility of the maximum diversification portfolio is 8%\n"
+            f"\nNominal (or face) value volatility indicates "
+            f"investing portfolio is {nominal_diversity}.\n"
+            f"Based on two criterias: \t 1) "
+            f"Investments split on to {classes[1]} asset class(es)\n"
+            f" \t \t \t \t 2) Total number of investments is {stakes}, "
+            f"when optimum for maximum diversification is 30."
+            )
+
+print(diversity(6))
