ca33d94b9f755d8561ffc94a101e031b046ee261
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-14 17:52:26 +0100

    SEARCH client by any data known from view

---
+++ portfolios/advanced/views.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE VIEW client_comp AS
+SELECT clients.c_id                         AS `Client id`,
+       clients.c_name                       AS `Client name`,
+       clients.c_mobile                     AS `Mobile`,
+       clients.c_email                      AS `Email`,
+       CONCAT ( clients.contract_type,
+       ' contract, max profits in ',
+       clients.investment_horizon,
+       ' years' )                           AS `Description`,
+       cid_platforms.platforms              AS `Platforms`,
+       aid_classes.asset_classes            AS `Invested Asset-Classes`
+FROM   clients,
+       (
+                SELECT   client_id                 AS cid_p,
+                         GROUP_CONCAT( DISTINCT(platform) ) AS platforms
+                FROM     investments
+                GROUP BY cid_p )
+     AS
+       cid_platforms,
+       (
+                  SELECT     client_id        AS cid_c,
+                             GROUP_CONCAT( DISTINCT(asset_class) ) AS asset_classes
+                  FROM       (assets
+                  INNER JOIN investments
+                  USING      (asset_id))
+                  GROUP BY   cid_c )
+
+     AS
+       aid_classes
+WHERE clients.c_id = cid_platforms.cid_p AND clients.c_id = aid_classes.cid_c
+ORDER BY clients.c_id;
