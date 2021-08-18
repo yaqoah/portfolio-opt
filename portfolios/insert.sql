af1d2c0dae02744cd53e8f6ee06a84677c49ff66
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-18 18:58:36 +0100

    Rearranged clients

--- portfolios/insert.sql
+++ portfolios/insert.sql
@@ -17,12 +17,13 @@ VALUES
     ('Commodity'), 
     ('Futures'), 
     ('Infrastructure');
+
 INSERT INTO clients
-	(c_name, c_age, c_mobile, c_email, contract_type)
+	(c_name, c_mobile, c_email, contract_type, investment_horizon)
 VALUES
 	('Sophia Joseph', '+1 312 649 5541', 'thesophiejo@gmail.com', 'flexible', 8),
     ('Faris Al-Zaabi', '+971 50 5472957', 'FarisAl@hotmail.com', 'long-term',15),
-    ('Chongxi Ma', '+852 2360 3382', 'xima@baidu.cn', 'long-term',30),
-    ('Williams Brown', '+1 718 963 0375', 'williambr@brown.edu', 'short-term',3),
     ('Hazlina Wong', '+65 8458 2573', 'won8lina@gmail.com', 'flexible',6),
+    ('Williams Brown', '+1 718 963 0375', 'williambr@brown.edu', 'short-term',3),
+    ('Chongxi Ma', '+852 2360 3382', 'xima@baidu.cn', 'long-term',30),
     ('Mike Karlsson', '+1 617 918 4243', 'Mikesson-pr1v@gmail.com', 'long-term',10);
